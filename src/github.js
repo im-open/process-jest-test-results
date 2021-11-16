08ec580c0e63f2e477fead34b2cb7fceb31ec202
Danielle Adams <danielle.adams3@extendhealth.com>
2021-11-16 11:56:54 -0700

    ARCH-1107 - Have the action retrieve all the PR comments instead of the default 30.

--- src/github.js
+++ src/github.js
@@ -39,19 +39,48 @@ async function createStatusCheck(repoToken, markupData, conclusion, reportName) 
 }
 
 async function lookForExistingComment(octokit) {
-  const commentsResponse = await octokit.rest.issues.listComments({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: github.context.payload.pull_request.number
-  });
+  let hasMoreComments = true;
+  let page = 1;
+  const maxResultsPerPage = 30;
+  let prComments = [];
 
-  if (commentsResponse.status !== 200) {
-    core.info(`Failed to list PR comments. Error code: ${commentsResponse.status}.  Will create new comment instead.`);
-    return null;
+  while (hasMoreComments) {
+    const commentsResponse = await octokit.rest.issues.listComments({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.payload.pull_request.number,
+      per_page: maxResultsPerPage,
+      page: page
+    });
+
+    if (commentsResponse.status == 200) {
+      if (commentsResponse.data) {
+        if (commentsResponse.data.length < maxResultsPerPage) {
+          hasMoreComments = false;
+        } else {
+          page += 1;
+        }
+
+        for (let index = 0; index < commentsResponse.data.length; index++) {
+          const comment = commentsResponse.data[index];
+          prComments.push({
+            id: comment.id,
+            body: comment.body
+          });
+        }
+      }
+    } else {
+      core.info(`Failed to list PR comments. Error code: ${commentsResponse.status}.  Will create new comment instead.`);
+      return null;
+    }
   }
 
-  const existingComment = commentsResponse.data.find(c => c.body.startsWith(markupPrefix));
-  if (!existingComment) {
+  core.info(`Finished getting comments for PR #${github.context.payload.pull_request.number}.`);
+
+  const existingComment = prComments.find(c => c.body.startsWith(markupPrefix));
+  if (existingComment) {
+    core.info(`An existing jest test results comment was found.`);
+  } else {
     core.info('An existing jest test results comment was not found, create a new one instead.');
   }
   return existingComment ? existingComment.id : null;
